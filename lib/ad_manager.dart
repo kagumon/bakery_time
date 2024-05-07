e002b303fd7cb643c060c3e8e0a70d702bc49830
kagumon <kyung2687@gmail.com>
2024-05-07 21:46:09 +0900

    광고 배너 개발

--- lib/ad_manager.dart
+++ lib/ad_manager.dart
@@ -1,27 +1,23 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-
 class AdManager {
-  // singleton instance
   static AdManager instance = AdManager();
-  
   AdManagerBannerAd? bannerAd;
-  
+
   AdManager({
     this.bannerAd,
   });
-  
-  // AdManager 객체 초기화
+
   factory AdManager.init() => instance = AdManager(
     bannerAd: _loadBannerAd(),
   );
 }
 
-// AdManagerBannerAd 객체를 로드하는 함수
 AdManagerBannerAd _loadBannerAd() {
   const String androidBannerAdUnitId = 'ca-app-pub-6146515360845106/6478226863';
-  const String iosBannerAdUnitId = 'ca-app-pub-6146515360845106/7081943668';
+  const String iosBannerAdUnitId = kDebugMode ? 'ca-app-pub-3940256099942544/2934735716' : 'ca-app-pub-6146515360845106/7081943668';
 
   String adUnitId = androidBannerAdUnitId;
   if(Platform.isIOS) adUnitId = iosBannerAdUnitId;
